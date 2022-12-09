4711d3d94d9adb88507150690bc4b5459bd3fe1c
mxgncraig <megansmcraig@gmail.com>
2022-12-09 17:46:50 +1000

    Fixed js link bug

--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
-function displayTemperature(response) {
-  console.log(response.data);
-}
 let apiKey = "823db6e133aat4d41o6f0ce4dc3ba6ce";
 let city = "New York";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.Get(apiUrl).then(displayTemperature);
+console.log(apiUrl);
