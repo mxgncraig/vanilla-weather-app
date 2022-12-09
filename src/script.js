bbfc24d1adbefc4f085f913804ce4dad9253c030
mxgncraig <megansmcraig@gmail.com>
2022-12-09 17:40:39 +1000

    Testing API data

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 let apiKey = "823db6e133aat4d41o6f0ce4dc3ba6ce";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+let city = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.Get(apiUrl).then(displayTemperature);
