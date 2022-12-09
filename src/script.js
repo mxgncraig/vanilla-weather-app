e2b3fd1676530e30a81869939c3347c9d440205b
mxgncraig <megansmcraig@gmail.com>
2022-12-09 18:18:43 +1000

    Created main api call

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
+function showTemperature(response) {
+  console.log(response.data.temperature);
+  let cityElement = document.querySelector("#city-name");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelsElement = document.querySelector("#feels-like");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelsElement.innerHTML = Math.round(response.data.temperature.feels_like);
+}
+
 let apiKey = "823db6e133aat4d41o6f0ce4dc3ba6ce";
 let city = "New York";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+axios.get(apiUrl).then(showTemperature);
