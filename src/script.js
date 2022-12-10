6237b93a01152877185e07abe60b4ca720a4d735
mxgncraig <megansmcraig@gmail.com>
2022-12-10 15:32:14 +1000

    Updated with current icon api

--- src/script.js
+++ src/script.js
@@ -8,8 +8,9 @@ if (hours < 10) {
 }
 let minutes = now.getMinutes();
 if (minutes < 10) {
-  minutes = `0{minutes}`;
+  minutes = `0${minutes}`;
 }
+
 timeElement.innerHTML = `${day} ${hours}:${minutes}`;
 function showTemperature(response) {
   let cityElement = document.querySelector("#city-name");
@@ -17,14 +18,19 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let feelsElement = document.querySelector("#feels-like");
+  let currentIcon = document.querySelector("#current-icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   feelsElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  currentIcon.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
 
 let apiKey = "823db6e133aat4d41o6f0ce4dc3ba6ce";
-let city = "New York";
+let city = "Brisbane";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
